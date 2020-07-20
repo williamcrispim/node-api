39ae65b4eb871ad00198f2c81a8f1fa364d633ba
William Crispim <williamcrispim@live.com>
2020-07-20 20:30:52 -0300

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
+const cors = require("cors")
 
 // Iniciando o app
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Iniciando o DB
